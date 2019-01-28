81862579480e290e94bba76b8a9395ff82f8d163
David Acosta <davidacosta25081@yahoo.com>
2019-01-28 21:56:20 +0000

    Done.

--- countdown_with_sleep.rb
+++ countdown_with_sleep.rb
@@ -1,14 +1,15 @@
 
 
-def countdown(counter)
+def countdown_with_sleep(counter)
 
   while counter > 0
     puts "#{counter} SECOND(S)!"
     counter -= 1
+sleep (1)
   end
-puts "Happy New year !!!"
+puts "Happy New Year!!! "
 "HAPPY NEW YEAR!"
 
 end
 
-countdown(50)
+countdown_with_sleep(10)
